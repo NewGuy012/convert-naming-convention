4952f09b042d990018c7f42dd34fb7adf68b4bf9
Moses Yoo <juyoung.m.yoo@gmail.com>
2023-03-13 22:31:41 -0700

    Swap camel vs pascal case code.

--- snake2camel.m
+++ snake2camel.m
@@ -2,7 +2,7 @@ function [camelCase, PascalCase] = snake2camel(snake_case)
 %snake2camel Converts string array from snake case to camel/pascal case
 %
 % Syntax:
-%   camelCase = snake2camel(snake_case)
+%   [camelCase, PascalCase] = snake2camel(snake_case)
 %
 % Input Arguments:
 %   snake_case - Input text in snake case
@@ -23,8 +23,8 @@ temp_txt = regexprep(snake_case,'_([a-z])','${upper($1)}');
 temp_txt = regexprep(temp_txt,'_([0-9])', '$1');
 
 % Camel case
-camelCase = regexprep(temp_txt, '^([a-z])', '${upper($1)}');
+camelCase = regexprep(temp_txt, '^([A-Z])', '${lower($1)}');
 
 % Pascal case
-PascalCase = regexprep(temp_txt, '^([A-Z])', '${lower($1)}');
+PascalCase = regexprep(temp_txt, '^([a-z])', '${upper($1)}');
 end
